669ced034a4f56ac9332277935cd74a8e98ca413
Grigory Alexandrov <alexaandrov@gmail.com>
2016-12-18 23:14:04 +0300

    Fixed URL link in task menu

--- views/task/index.php
+++ views/task/index.php
@@ -9,7 +9,7 @@ $this->title = 'All Tasks';
     <h1>All Tasks</h1>
 
     <ul class="tasks list-group">
-        <li class="list-group-item"><a href="1">Task 1</a></li>
-        <li class="list-group-item"><a href="2">Task 2</a></li>
+        <li class="list-group-item"><a href="/task/1">Task 1</a></li>
+        <li class="list-group-item"><a href="/task/2">Task 2</a></li>
     </ul>
 </div>
